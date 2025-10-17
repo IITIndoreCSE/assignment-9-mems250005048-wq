634134f0ede3f858caf4748762bbe6b8374d99dd
mems250005048-wq <mems250005048@iiti.ac.in>
2025-10-17 15:49:19 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,3 +1,20 @@
 #include iostream;
 using namespace std;
-struct 
+struct Item{
+int id;
+float cost;
+};
+int main(){
+  Item items[6];
+items[0]={109,69};
+items[1]={100,43.5};
+items[2]={110,69.5};
+items[3]={150,50.5};
+cout<<"Items with cost greater than 50:\n";
+for(int i=0;i<6;i++){
+if items[i].cost>50){
+cout<<"Item ID: "<<item[i].id<<", Cost: "<<items[i].cost<<endl;
+}
+}
+return 0;
+}
