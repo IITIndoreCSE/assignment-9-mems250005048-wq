7953f5d34bb36cddf94f2882cf7b475b6a479a6d
mems250005048-wq <mems250005048@iiti.ac.in>
2025-10-17 16:27:57 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,16 +1,19 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 struct Point{
-flooat x;
+float x;
 float y;
 };
 int main(){
   Point points[7];
   int a=0;
 for(int i =1,i<8, i++){
-  cout<<"Point "<<i;
+  cout<<"Point "<<i<<" x: ";
+  cout<<"Point "<<i<<" y: ";
   for(int i=0;i<7;i++){
-cin>>points[i];
+cin>>points[i].x>>points[i].y;
+
+
 }
 }
 for(int i=0;i<7;i++){
