d7d029581ad8becc562f94890fc43083231b6372
mems250005048-wq <mems250005048@iiti.ac.in>
2025-10-17 16:31:02 +0530

    Fix input loop for point coordinates
    Refactor input loop for points to improve clarity and structure.

--- Q3.cpp
+++ Q3.cpp
@@ -7,14 +7,16 @@ float y;
 int main(){
   Point points[7];
   int a=0;
-for(int i =1,i<8, i++){
-  cout<<"Point "<<i<<" x: ";
-  cout<<"Point "<<i<<" y: ";
-  for(int i=0;i<7;i++){
-cin>>points[i].x>>points[i].y;
+for(int i =0;i<7;i++){
+  cout<<"Point "<<i+1<<" x: ";
+  cin>>points[i].x;
+  
+  cout<<"Point "<<i+1<<" y: ";
+  
+cin>>points[i].y;
 
 
-}
+
 }
 for(int i=0;i<7;i++){
 if(points[i].x >0 && points[i].y > 0){
